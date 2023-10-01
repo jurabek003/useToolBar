0ce8085c9d8c84325db3af85ed1342b3dd7eb34c
Asus <turgunboyevjurabek777@gmail.com>
2023-10-01 23:55:51 +0500

    viewBinding added

--- app/src/main/java/uz/turgunboyevjurabek/myapplication/MainActivity.java
+++ app/src/main/java/uz/turgunboyevjurabek/myapplication/MainActivity.java
@@ -3,13 +3,18 @@ package uz.turgunboyevjurabek.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import uz.turgunboyevjurabek.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
 
     }
 }
